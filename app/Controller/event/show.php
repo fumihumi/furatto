2e027f9426595900ebc16e2add4a031894b89ec9
fumihumi <t.fum1hum@gmail.com>
2018-09-11 16:18:13 +0900

    bugfix: user_idを取得してなかった:

--- app/Controller/event/show.php
+++ app/Controller/event/show.php
@@ -11,7 +11,7 @@ $app->get('/event/{id}', function (Request $request, Response $response, $args) 
     $user = new User($this->db);
 
     $data['event'] = $event->select(array("id" => $args['id']));
-    $data['user'] = $user->select(array("id" => $data['event']['id']));
+    $data['user'] = $user->select(array("id" => $data['event']['user_id']));
 
     return $this->view->render($response, 'event/show.twig', $data);
 });
