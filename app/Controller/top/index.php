1300710ee92418b45abca656105c87358c717a46
SATOH Ryota <pfds---@outlook.jp>
2018-09-11 13:58:14 +0900

    top画面

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,15 +2,24 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\Event;
 use Model\Dao\User;
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
-    $data = [];
-    $event = new User($this->$db);
+    $events_dao = new Event($this->db);
+    $users_dao = new User($this->db);
+    $events = $events_dao->select([], "startDate", "DESC", 10, true);
 
-    // $result = $event->select([], "startDate", "DESC", 10, true);
+    // 登録されたeventごとに紐づけられたuserのnameを取り出し，hostに格納する
+    foreach($events as $key => $event)
+    {
+      $param["id"] = $event["user_id"];
+      $events[$key]['host'] = $users_dao->select($param, "", "", 1, false);
+    }
+
+    $data["events"] = $events;
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
