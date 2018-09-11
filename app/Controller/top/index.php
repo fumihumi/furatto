7c90dfb3796bc0c49b9e78c0c1bfbcd0725dd4be
SATOH Ryota <pfds---@outlook.jp>
2018-09-11 12:11:23 +0900

    topのcontroller

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,13 +2,16 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\User;
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
     $data = [];
+    $event = new User($this->$db);
+
+    // $result = $event->select([], "startDate", "DESC", 10, true);
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
 });
-
