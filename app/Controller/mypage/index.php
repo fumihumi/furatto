dbcbe818f17c43cab93339c48a0cd9756cd427d0
Kazuya-Furusho <k.ktm0813@icloud.com>
2018-09-12 10:41:14 +0900

    /user/'id'ページ追加

--- app/Controller/mypage/index.php
+++ app/Controller/mypage/index.php
@@ -2,32 +2,29 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\User;
+use Model\Dao\Event;
+use Model\Dao\EventUser;
 // mypageページのコントローラ
 $app->get('/mypage/', function (Request $request, Response $response) {
 
      $user = new User($this->db);
      $currentUserId = $this->session->user_info["id"];
 
-     // $param, "", "", 1,false
      $data["user"] = $user->select(array("id" => $currentUserId));
 
     return $this->view->render($response, 'mypage/index.twig', $data);
 });
 
 $app->get('/user/{id}', function (Request $request, Response $response, $args) {
-
     $user = new User($this->db);
+    $event = new Event($this->db);
+    $eventuser= new EventUser($this->db);
+    $user_id= $args['id'];
 
-    $data["user"] = $user->select(array("id" => $args));
+      $data["user"] = $user->select(array("id" => $user_id));
+
+      $data["event"]=$eventuser->select(array("user_id" => $event));
 
     return $this->view->render($response, 'mypage/index.twig', $data);
-});
 
-// $app->get('/event/{id}', function (Request $request, Response $response, $args) {
-//
-//     $user = new User($this->db);
-//
-//     $data = $user->select(array("id" => $args));
-//
-//     return $this->view->render($response, 'mypage/index.twig', $data);
-// });
+});
