bbe3fad530069c907cf4d853285099fbc254c76a
fumihumi <t.fum1hum@gmail.com>
2018-09-12 11:50:20 +0900

    ボタン押したらイベントに参加するよー

---
+++ app/Controller/event/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\Event;
+use Model\Dao\User;
+use Model\Dao\EventUser;
+
+$app->post('/event/{id}/participate/', function (Request $request, Response $response, $args) {
+
+    $eventId = $args["id"];
+
+    $params = $request->getParsedBody();
+
+    $userInfo = $this->session["user_info"];
+    $currentUserId = $userInfo['id'];
+
+    // ここで参加する処理する
+    // flg == 1 だったら参加して０なら削除する??????
+    // 一旦参加だけなので FIXME
+
+    $flg = $params['participate'];
+
+    if ($flg == 1) {
+        $eventUserData = array(
+            'user_id' => $currentUserId,
+            'event_id' => $eventId
+        );
+    }
+
+    $eventUsers = new EventUser($this->db);
+    $eventUsers->insert($eventUserData);
+
+    return $response->withRedirect("/event/".$eventId);
+});
